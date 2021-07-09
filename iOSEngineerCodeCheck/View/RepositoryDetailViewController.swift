84009193587dc11a8d98ee9e3b0070ab5ce13938
hirossan4049 <haruto405329@gmail.com>
2021-07-09 16:14:00 +0900

    RepositoryDetailVCのUIを少しよくした2。

--- iOSEngineerCodeCheck/View/RepositoryDetailViewController.swift
+++ iOSEngineerCodeCheck/View/RepositoryDetailViewController.swift
@@ -13,12 +13,13 @@ class RepositoryDetailViewController: UIViewController {
     @IBOutlet weak var avatarImageView: UIImageView!
 
     @IBOutlet weak var titleLabel: UILabel!
-
+    @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var languageLabel: UILabel!
     @IBOutlet weak var starsCountLabel: UILabel!
     @IBOutlet weak var watchersCountLabel: UILabel!
     @IBOutlet weak var forksCountLabel: UILabel!
     @IBOutlet weak var issuesCountLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
 
     var repository: Repository?
 
@@ -30,17 +31,23 @@ class RepositoryDetailViewController: UIViewController {
     
     private func setupView() {
         navigationItem.largeTitleDisplayMode = .never
-        title = repository?.name
+        title = "\(repository?.owner?.username ?? "")/\(repository?.name ?? "")"
         
-        titleLabel.text = "\(repository?.owner?.username ?? "")/\(repository?.name ?? "")"
+        titleLabel.text = "\(repository?.name ?? "")"
+        usernameLabel.text = "\(repository?.owner?.username ?? "")"
         languageLabel.text = "Written in \(repository?.language ?? "")"
         starsCountLabel.text = "\(repository?.starCount ?? 0) stars"
         watchersCountLabel.text = "\(repository?.watcherCount ?? 0) watchers"
         forksCountLabel.text = "\(repository?.forkCount ?? 0) forks"
         issuesCountLabel.text = "\(repository?.issueCount ?? 0) open issues"
+        descriptionTextView.text = "\(repository?.description ?? "")"
+        
+        usernameLabel.textColor = .tertiaryLabelColor
         
         view.backgroundColor = .backgroundColor
         
+        descriptionTextView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+                
         getImage()
     }
 
